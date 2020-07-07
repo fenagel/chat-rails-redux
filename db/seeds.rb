5f29d957ded113a94090d0bb7670c605ff46ab68
Felix Nagel <felixna7@gmail.com>
2020-07-07 18:20:51 +0200

    messages_list still bugged

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Message.destroy_all
+User.destroy_all
+Channel.destroy_all
+
+channel_one = Channel.create!(name: 'general')
+channel_two = Channel.create!(name: 'react')
+channel_three = Channel.create!(name: 'berlin')
+channel_four = Channel.create!(name: 'rails')
+
+user = User.create!(email:'bob@example.com', password: 123456, nickname: 'Bob')
+
+Message.create!(content: 'Hi, this is Bob!', user: user, channel: channel_one)
+Message.create!(content: 'Hi, this is Bob!', user: user, channel: channel_two)
+Message.create!(content: 'Hi, this is Bob!', user: user, channel: channel_three)
+Message.create!(content: 'Hi, this is Bob!', user: user, channel: channel_four)
